ef3bd9a3ee675d5fd7ca0ad6c60c36df50388523
Yuriko Hijikata <akanesasu3@gmail.com>
2021-06-18 16:32:29 +0900

    fixed the pre-initialization error

--- utils/scraper.js
+++ utils/scraper.js
@@ -9,14 +9,21 @@ const getProductInfo = async (url) => {
   const chromeOptions = {
     headless: true,
     defaultViewport: null,
-    args: ["--incognito", "--no-sandbox", "--single-process", "--no-zygote", "--lang=ja"],
+    args: [
+      "--incognito",
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--single-process",
+      "--no-zygote",
+      "--lang=ja",
+    ],
   };
   const browser = await puppeteer.launch(chromeOptions);
+
+  const page = await browser.newPage();
   await page.setUserAgent(
     "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
   );
-
-  const page = await browser.newPage();
   // console.log("launched");
 
   try {
