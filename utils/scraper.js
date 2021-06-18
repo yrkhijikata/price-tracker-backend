af296e3ed98228d100600045f5eb434d71d80b8e
Yuriko Hijikata <akanesasu3@gmail.com>
2021-06-18 16:26:34 +0900

    added userAgent

--- utils/scraper.js
+++ utils/scraper.js
@@ -12,6 +12,10 @@ const getProductInfo = async (url) => {
     args: ["--incognito", "--no-sandbox", "--single-process", "--no-zygote", "--lang=ja"],
   };
   const browser = await puppeteer.launch(chromeOptions);
+  await page.setUserAgent(
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
+  );
+
   const page = await browser.newPage();
   // console.log("launched");
 
