6276f1b90bcf1f0d101876c268bbc9c6d51c5ab3
Yuriko Hijikata <akanesasu3@gmail.com>
2021-06-19 23:56:36 +0900

    added cors

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const { getProductInfo } = require("./utils/scraper");
 const { autoscraper } = require("./utils/autoscraper");
+var cors = require("cors");
 
 const app = express();
+app.use(cors());
 
 const port = process.env.PORT || 5000;
 app.use(express.urlencoded({ extended: false }));
@@ -26,4 +28,4 @@ app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
 
-// autoscraper();
+autoscraper();
